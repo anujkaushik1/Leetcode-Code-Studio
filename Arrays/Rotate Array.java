c6634dd9c78646d70fe9e314cee27bc23ae34a1a
Anuj Kaushik <66275103+anujkaushik1@users.noreply.github.com>
2023-01-11 18:53:18 +0530

    Create Rotate Array.java

---
+++ Arrays/Rotate Array.java
@@ -0,0 +1,16 @@
+// Space - O(nums.length)
+// Time - o(nums.length)
+class Solution {
+    public void rotate(int[] nums, int k) {
+        
+        int ans[] = new int[nums.length];
+
+        for(int i = 0; i < nums.length; i++)
+            ans[(i + k) % nums.length] = nums[i]; 
+
+        for(int i = 0; i < ans.length; i++)
+            nums[i] = ans[i];
+
+
+    }
+}
